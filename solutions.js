4d904fa809e1a3e6c1c9e48bf9bc849011a18e67
Daniel Ji <daniel.ji0923@gmail.com>
2023-11-11 19:36:53 -0800

    verbal questions completed

---
+++ solutions.js
@@ -0,0 +1,4 @@
+// What is the difference between a parameter and an argument?
+// Within a function, what is the difference between return and console.log?
+
+// a parameter are the names that go inside the function () and an arugment is the actual value that is being passed through
